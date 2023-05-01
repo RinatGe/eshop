0510b327b5d5b6eb5e0b339571846bb97c9b08e6
RinatGe <wallag4@walla.com>
2023-05-01 13:28:12 +0300

    before try to global imports

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,10 +5,27 @@ import expressAsyncHandler from 'express-async-handler';
 const userRouter = express.Router();
 
 userRouter.post(
-  'signin',
+  '/signin',
   expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcryt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({message: 'Invalid Password/User'});
   })
 );
+
+
+
+
+
 
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,8 +15,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());//making sure data coming trensfers to json
 app.use(express.urlencoded({extended: true}));//arrange cors to go thro domains
+app.use('/api/v1/users',userRouter);
 app.use('/api/v1/seed', seedRouter);
 app.use('/api/v1/products', productRouter);
+
+//middlewere,this is how we work with express handler-if //there is an error it will comne here and get response
+
+app.use((err, req, res, next) => { 
+  res.status(500).send({message: err.message});
+})
 
 //Endpoints
 
